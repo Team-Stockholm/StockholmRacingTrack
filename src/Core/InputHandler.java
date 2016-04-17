cf265b4ca83c7a817f43cfd580d44b29c367c1a7
Yovko Gospodinov <yovko.gospodinov@gmail.com>
2016-04-17 21:28:10 +0300

    changing the command to start the game from space button to mouse click.

--- src/Core/InputHandler.java
+++ src/Core/InputHandler.java
@@ -3,17 +3,48 @@ package Core;
 
 import Display.Display;
 import States.GameState;
+import States.MainMenuState;
 import States.State;
 import States.StateManager;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class InputHandler implements KeyListener{
 
     public InputHandler(Display display) {
         display.getCanvas().addKeyListener(this);
+        display.getCanvas().addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(StateManager.getCurrentState());
+                if (StateManager.getCurrentState().toString().contains("MainMenuState")){
+                    StateManager.setCurrentState(new GameState() {
+                    });}
+            }
 
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
     }
 
     @Override
@@ -29,11 +60,13 @@ public class InputHandler implements KeyListener{
             GameState.player.isMovingRight = true;
         }else if (keyCode == KeyEvent.VK_LEFT){
             GameState.player.isMovingLeft = true;
-        }else if (keyCode == KeyEvent.VK_SPACE) {
-            if (StateManager.getCurrentState().toString().contains("MainMenuState")) {
-                StateManager.setCurrentState(new GameState());
-            }
         }
+//        Loading a new GameState with pressing space - starting game.
+//        else if (keyCode==KeyEvent.VK_SPACE) {
+//            if (StateManager.getCurrentState().toString().contains("MainMenuState")){
+//                StateManager.setCurrentState(new GameState() {
+//                });}
+//        }
     }
 
     @Override
